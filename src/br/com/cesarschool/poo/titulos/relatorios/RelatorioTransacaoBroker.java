da752226ba94d7d07f78ceecf0225fa85786c3f8
Gheyson Melo <gheysonmelo@hotmail.com>
2024-11-25 12:16:41 -0300

    resolving last test

--- src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
+++ src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
@@ -2,46 +2,52 @@ package br.com.cesarschool.poo.titulos.relatorios;
 
 import br.com.cesarschool.poo.titulos.entidades.Transacao;
 import br.com.cesarschool.poo.titulos.repositorios.RepositorioTransacao;
+import br.com.cesarschool.poo.titulos.utils.ComparadorPorDataHora;
 import br.com.cesarschool.poo.titulos.utils.ComparadorTransacaoPorNomeCredora;
 import br.com.cesarschool.poo.titulos.utils.Ordenador;
-import br.com.cesarschool.poo.titulos.utils.ComparadorPorDataHora;
 import br.gov.cesarschool.poo.daogenerico.Entidade;
 
 public class RelatorioTransacaoBroker {
-    RepositorioTransacao repositorioTransacao = new RepositorioTransacao();
+
+    private final RepositorioTransacao repositorioTransacao;
+
+    public RelatorioTransacaoBroker() {
+        this.repositorioTransacao = new RepositorioTransacao();
+    }
+
+    public RelatorioTransacaoBroker(RepositorioTransacao repositorioTransacao) {
+        this.repositorioTransacao = repositorioTransacao;
+    }
 
     public Transacao[] relatorioTransacaoPorNomeEntidadeCredora() {
-        // Obtem todas as entidades
-        Entidade[] entidades = repositorioTransacao.buscarTodos();
-
-        // Converte entidades para Transacao, validando o tipo
-        Transacao[] transacoes = converterParaTransacoes(entidades);
-
-        ComparadorTransacaoPorNomeCredora comparador = new ComparadorTransacaoPorNomeCredora();
-        Ordenador.ordenar(transacoes, comparador);
-
+        Transacao[] transacoes = obterTransacoesDoRepositorio();
+        Ordenador.ordenar(transacoes, new ComparadorTransacaoPorNomeCredora());
         return transacoes;
     }
 
     public Transacao[] relatorioTransacaoPorDataHora() {
-        // Obtem todas as entidades
-        Entidade[] entidades = repositorioTransacao.buscarTodos();
-
-        // Converte entidades para Transacao, validando o tipo
-        Transacao[] transacoes = converterParaTransacoes(entidades);
-
-        // Ordena as transações pela data e hora
-        ComparadorPorDataHora comparador = new ComparadorPorDataHora();
-        Ordenador.ordenar(transacoes, comparador);
-
+        Transacao[] transacoes = obterTransacoesDoRepositorio();
+        ComparadorPorDataHora ctpdh = new ComparadorPorDataHora();
+        for (int i = 1; i < transacoes.length; i++) {
+            Transacao chave = transacoes[i];
+            int j = i - 1;
+            while (j >= 0 && ctpdh.comparar(transacoes[j], chave) < 0) {
+                transacoes[j + 1] = transacoes[j];
+                j--;
+            }
+            transacoes[j + 1] = chave;
+        }
         return transacoes;
     }
 
-    // Método auxiliar para converter Entidade[] para Transacao[]
+    private Transacao[] obterTransacoesDoRepositorio() {
+        return converterParaTransacoes(repositorioTransacao.buscarTodos());
+    }
+
     private Transacao[] converterParaTransacoes(Entidade[] entidades) {
         return java.util.Arrays.stream(entidades)
-                .filter(Transacao.class::isInstance) // Filtra apenas objetos do tipo Transacao
-                .map(Transacao.class::cast) // Faz o cast seguro para Transacao
-                .toArray(Transacao[]::new); // Converte para um array de Transacao
+                .filter(Transacao.class::isInstance)
+                .map(Transacao.class::cast)
+                .toArray(Transacao[]::new);
     }
 }
